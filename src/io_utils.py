4b255dce841158214803260b87492d70bd56e05b
axiomcura <erikishere3@gmail.com>
2024-12-16 16:40:01 -0700

    added load_barcode() function

--- src/io_utils.py
+++ src/io_utils.py
@@ -2,12 +2,15 @@
 
 This module will contain functions pertaining to loading and writing files.
 """
-import pathlib
 
+import pathlib
+from collections import defaultdict
+
+import pandas as pd
 import yaml
 
 
-def load_config(fpath: str | pathlib.Path ) -> dict:
+def load_config(fpath: str | pathlib.Path) -> dict:
     """Loads in configuration file if specified path
 
     Parameters
@@ -33,3 +36,53 @@ def load_config(fpath: str | pathlib.Path ) -> dict:
     # next is to load the yaml file
     with open(fpath) as content:
         return yaml.safe_load(content)
+
+
+def load_barcodes(barcode_path: str | pathlib.Path) -> dict:
+    """
+    Load barcode data from a given file and organize it into batches.
+
+    This function reads barcode data, groups it by the 'platemap_file' column, and
+    organizes the grouped data into batches. Each batch is assigned a unique batch ID
+    (e.g., 'batch_1', 'batch_2', etc.).
+
+    Parameters
+    ----------
+    barcode_path : str | pathlib.Path
+        Path to the file containing barcode data. This path can be a string or a pathlib.Path
+        object.
+
+    Returns
+    -------
+    dict
+        A dictionary where keys are batch IDs (e.g., "batch_1", "batch_2") and values are
+        dictionaries.  Each inner dictionary maps a 'platemap_file' name to the
+        corresponding 'plate_barcode' data.
+    """
+
+    # type checking
+    if not isinstance(barcode_path, (str | pathlib.Path)):
+        raise TypeError("'barcode_path' must a str or pathlib.Path")
+    if isinstance(barcode_path, str):
+        barcode_path = pathlib.Path(barcode_path)
+
+    # load barcode as csv
+    barcodes = pd.read_csv(barcode_path)
+
+    # Initialize an empty dictionary to store barcode contents
+    barcode_contents = defaultdict(lambda: None)
+
+    # Group barcodes by 'platemap_file' and iterate through each group
+    for idx, (platemap_name, df) in enumerate(
+        barcodes.groupby("platemap_file"), start=1
+    ):
+        # Generate a batch ID
+        batch_id = f"batch_{idx}"
+
+        # Collect plate barcodes for the current platemap
+        batch_content = {platemap_name: df["plate_barcode"]}
+
+        # Store the batch content in the main dictionary
+        barcode_contents[batch_id] = batch_content
+
+    return barcode_contents
